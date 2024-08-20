d8e6c4b6957681009b91cc2ee3f1b6d125e94230
unknown <Darshan R>
2024-08-20 13:36:19 +0530

    changed

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+
+// const app = express();
+
+// // mongoose.connect("mongodb://localhost:27017/todo_express", {
+// //   useNewUrlParser: true,
+// //   UseUnifiedTopology: true,
+// // });
+// mongoose.connect('mongodb://localhost:27017/todo_express');
+
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static("public"));
+// app.set("view engine", "ejs");
+
+// app.use(require("./routes/index"));
+// app.use(require("./routes/todo"));
+
+// app.listen(5000, () => console.log("port running succesfully"));
+
 const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
 
-// mongoose.connect("mongodb://localhost:27017/todo_express", {
-//   useNewUrlParser: true,
-//   UseUnifiedTopology: true,
-// });
-mongoose.connect('mongodb://localhost:27017/todo_express');
-
+// Connect to MongoDB using the updated connection string format
+mongoose.connect("mongodb://localhost:27017/todo_express");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -17,4 +34,4 @@ app.set("view engine", "ejs");
 app.use(require("./routes/index"));
 app.use(require("./routes/todo"));
 
-app.listen(5000, () => console.log("port running succesfully"));
+app.listen(5000, () => console.log("Server running successfully on port 5000"));
